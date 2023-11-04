82d30af5a28c3896904bc69975d93a758fc1f318
Jorge Molina <138607757+Jmo-101@users.noreply.github.com>
2023-11-04 00:30:51 -0400

    Update main.tf

--- initTerraform/main.tf
+++ initTerraform/main.tf
@@ -6,8 +6,8 @@ provider "aws" {
 }
 
 provider "aws" {
-  access_key = var.access_key
-  secret_key = var.secret_key
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
   alias = "west"
   region     = "us-west-2"
 }
